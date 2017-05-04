806e922786e494830820d8945619e5a9aa30b9d8
ubuntu <ubuntu@ubuntu.(none)>
2017-05-03 22:31:01 -0400

    removed default get

--- autotest.sh
+++ autotest.sh
@@ -6,10 +6,11 @@ rm ftp/videos/hof/* &> /dev/null
 python ftp/ftpserver.py &> /dev/null &
 FTP_PID=$!
 
+MYSQL_URI=''
 CSRF_KEY=$(head -c 16 /dev/urandom)
 SECRET_KEY=$(head -c 16 /dev/urandom)
 
-PURVIEW_CSRF_SESSION_KEY=$CSRF_KEY PURVIEW_SECRET_KEY=$SECRET_KEY python -m unittest discover tests
+PURVIEW_MYSQL_URI=$MYSQL_URI PURVIEW_CSRF_SESSION_KEY=$CSRF_KEY PURVIEW_SECRET_KEY=$SECRET_KEY python -m unittest discover tests
 
 kill -KILL $FTP_PID &> /dev/null
 
